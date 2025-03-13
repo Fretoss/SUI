c13e84ac3fc77639f7c43f5a69cdaa88548bf1d6
Deni <valiev10266@gmail.com>
2025-03-13 11:31:40 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRPO_GIT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HALA MADRID || Y NADA MAS || SUIIII");
+            Console.ReadLine();
+        }
+    }
+}
